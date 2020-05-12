0d753063d6e96e018762bb247cca13a257ca0f2f
VE3NEA <alshovk@dxatlas.com>
2020-05-12 10:55:42 -0400

    Upload DisplayPanelDemo

---
+++ VE3NEA.HamCockpitPlugins.DisplayPanelDemo/DisplayPanel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VE3NEA.HamCockpitPlugins.DisplayPanelDemo
+{
+  public partial class DisplayPanel: UserControl
+  {
+    public DisplayPanel()
+    {
+      InitializeComponent();
+    }
+
+    public DisplayPanelSettings Settings
+    {
+      get
+      {
+        return new DisplayPanelSettings(
+        toolStripButton1.Checked,
+        toolStripButton2.Checked,
+        toolStripButton3.Checked);
+      }
+      set 
+      {
+        toolStripButton1.Checked = value.Red;
+        toolStripButton2.Checked = value.Green;
+        toolStripButton3.Checked = value.Blue;
+        ButtonsToColor();
+      }
+    }
+
+    private void toolStripButton1_Click(object sender, EventArgs e)
+    {
+      ButtonsToColor();
+    }
+
+    void ButtonsToColor()
+    {
+      BackColor = Color.FromArgb(
+        toolStripButton1.Checked ? 255 : 180,
+        toolStripButton2.Checked ? 255 : 180,
+        toolStripButton3.Checked ? 255 : 180);
+    }
+  }
+
+
+  // this class is a container for all settings of a display panel
+  // that we want to be preserved between the program restarts.
+  // in this demo, we just store the three components of the panel color.
+  // each component is toggled with its toolbutton on the display panel.
+  public class DisplayPanelSettings
+  {
+    public bool Red { get; set; }
+    public bool Green { get; set; }
+    public bool Blue { get; set; }
+
+    public DisplayPanelSettings(bool red, bool green, bool blue)
+    {
+      Red = red;
+      Green = green;
+      Blue = blue;
+    }
+  }
+}
---
+++ VE3NEA.HamCockpitPlugins.DisplayPanelDemo/DisplayPanelDemo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VE3NEA.HamCockpit.PluginAPI;
+using System.Windows.Forms;
+using System.ComponentModel.Composition;
+
+namespace VE3NEA.HamCockpitPlugins.DisplayPanelDemo
+{
+  // the plugin class. It does not do anything interesting on its own, it just
+  // creates DisplayPanel's when asked, and keeps track of their settings.
+  [Export(typeof(IPlugin))]
+  [Export(typeof(IVisualPlugin))]
+  class DisplayPanelDemo : IPlugin, IVisualPlugin
+  {
+    private List<DisplayPanel> panels = new List<DisplayPanel>();
+    private Settings settings = new Settings();
+
+
+    //----------------------------------------------------------------------------------------------
+    //                                        IPlugin
+    //----------------------------------------------------------------------------------------------
+    public string Name => "Display Panel Demo";
+    public string Author => "VE3NEA"; //use your callsign if you have one.
+    public bool Enabled { get; set; }
+    
+    // we do not use these.
+    public ToolStrip ToolStrip => null;
+    public ToolStripItem StatusItem => null;
+
+    // let the host save and load our settings, and allow the user edit those that are 
+    // marked as browsable. No browsable settings in this demo though.
+    public object Settings { get => BuildSettings(); set => ApplySettings(value); }
+
+    // we will create as many panels as the host needs
+    public bool CanCreatePanel => true;
+
+
+    //----------------------------------------------------------------------------------------------
+    //                                      IDisplayPanel
+    //----------------------------------------------------------------------------------------------
+    public UserControl CreatePanel()
+    {
+      var new_panel = new DisplayPanel();
+
+      // on program startup the plugin receives its settings that were saved on program exit.
+      // In our case, they contain the settings of all display panels that were open.
+      // Now the host re-creates the panels, and we use the saved panels' settings
+      // when we are asked to create a panel. After all settings are consumed, we create panels
+      // with default settings.
+      if (settings.panels.Count > 0)
+      {
+        new_panel.Settings = settings.panels[0];
+        settings.panels.RemoveAt(0);
+      }
+
+      panels.Add(new_panel);
+      return new_panel;
+    }
+
+    public void DestroyPanel(UserControl panel)
+    {
+      // we keep track of all open panels so that we can generate our settings when asked by host.
+      panels.Remove(panel as DisplayPanel);
+    }
+
+
+    //----------------------------------------------------------------------------------------------
+    //                                    private methods
+    //----------------------------------------------------------------------------------------------
+    private void ApplySettings(object value)
+    {
+      // just store the settings. 
+      settings = value as Settings;
+    }
+
+    private object BuildSettings()
+    {
+      settings.panels.Clear();
+      foreach (var panel in panels) settings.panels.Add(panel.Settings);
+      return settings;
+    }
+  }
+
+
+
+  // this class is a container for all settings of the plugin
+  // that we want to be preserved between the program restarts.
+  // in this demo, we just store the settings of each open display panel
+  public class Settings
+  {
+    public List<DisplayPanelSettings> panels = new List<DisplayPanelSettings>();
+  }
+}
